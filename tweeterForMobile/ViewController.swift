79b82d09c7bb1d6b098573160f5285513bd2b902
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-08-07 17:29:50 +0900

    ヘッダーフッターの実装

--- tweeterForMobile/ViewController.swift
+++ tweeterForMobile/ViewController.swift
@@ -5,7 +5,39 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  @IBDesignable
+  class MyStackView: UIStackView {
+      
+      @IBInspectable
+      var color: UIColor?
+      
+      override var backgroundColor: UIColor? {
+          get {
+              return color
+          }
+          set {
+              color = newValue
+              setNeedsLayout()
+          }
+      }
 
+      private lazy var backgroundLayer: CAShapeLayer = {
+          let layer = CAShapeLayer()
+          self.layer.insertSublayer(layer, at: 0)
+          return layer
+      }()
+      
+      override func layoutSubviews() {
+          super.layoutSubviews()
+          
+          CATransaction.begin()
+          CATransaction.setDisableActions(true) // CALayerの暗黙的アニメーションは不要なのでオフにする
+          
+          backgroundLayer.path = UIBezierPath(rect: bounds).cgPath
+          backgroundLayer.fillColor = backgroundColor?.cgColor
 
+          CATransaction.commit()
+      }
+  }
 }
 
