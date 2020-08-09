eb00f2cf16b4f03afd57ad5328540920b8bd7778
Fumiya-Soeno <61449986+Fumiya-Soeno@users.noreply.github.com>
2020-08-09 10:19:31 +0900

    ツイート入力画面での自動フォーカス、placeholder

--- tweeterForMobile/TweetViewController.swift
+++ tweeterForMobile/TweetViewController.swift
@@ -2,11 +2,17 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+  override func viewDidLoad() {
+      super.viewDidLoad()
+  }
   
+  override func viewWillAppear(_ animated: Bool) {
+       TweetTextFieldOutlet.becomeFirstResponder()
+  }
+  
+  @IBOutlet weak var TweetTextFieldOutlet: UITextField!
   @IBAction func CancelButtonAction(_ sender: Any) {
     self.dismiss(animated: true, completion: nil)
   }
+  
 }
